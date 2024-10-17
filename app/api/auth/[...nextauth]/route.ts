63072b807c71e4795c46732972c7d5265340eb52
Raj Deshmukh <rajdeshmukh41705@gmail.com>
2024-10-17 12:37:37 +0530

    production login fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -50,7 +50,8 @@ const handler = NextAuth({
                 return false;
             }
         }
-    }
+    },
+    secret : process.env.NEXTAUTH_SECRET
 })
 
 export {handler as GET,handler as POST}
